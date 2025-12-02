5e23000d5197d503a6b99646117728f978fd77b0
Thomas Metais <thomas.metais@etu.univ-nantes.fr>
2025-12-02 22:43:13 +0100

    add tests for creerElementListe() & egalitePersonne()

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 /*
  * Nom1 Nom2
- * TP4 
- * 
+ * TP4
+ *
  * .......
- * 
+ *
  */
-
 
 #include <iostream>
 #include "utilitaire_generation.h"
 #include "type_def.h"
 #include "utilitaires.h"
 
-int main(){
-    personne* p1 = genererPersonne();
+int main()
+{
+    personne *p1 = genererPersonne();
     std::string nom = genererNomPrenom("Noms_TP4.txt", 1000);
     std::string prenom = genererNomPrenom("Prenoms_TP4.txt", 11612);
     std::string tel = genererTel();
-    cout<< nom<<" "<<prenom<<" "<<tel<<endl;
+    cout << nom << " " << prenom << " " << tel << endl;
+
+    // test creerElementListe
+    elementListe *elem = creerElementListe(*p1);
+    cout << elem->p.nom << " " << elem->p.prenom << " " << elem->p.telephone << endl;
+
+    // test egalitePersonne
+    personne *p2 = new personne{nom, prenom, tel};
+    if (egalitePersonne(*p1, *p2))
+    {
+        cout << "Les deux personnes sont égales." << endl;
+    }
+    else
+    {
+        cout << "Les deux personnes sont différentes." << endl;
+    }
+
     return 0;
 }
