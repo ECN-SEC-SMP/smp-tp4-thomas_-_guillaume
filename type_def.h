0e5f74099875fcf14fba58a9093ac39b146c3278
Thomas Metais <thomas.metais@etu.univ-nantes.fr>
2025-12-02 20:40:57 +0100

    add struct elementListe

--- type_def.h
+++ type_def.h
@@ -1,1 +1,8 @@
 #pragma once
+
+typedef struct elementListe
+{
+  personne personne;
+  struct elementListe *suivant;   // pointeur vers l'élément suivant
+  struct elementListe *precedent; // pointeur vers l'élément précédent
+} elementListe;
