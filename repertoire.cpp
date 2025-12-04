4bf7952a1ff2de99186c108bcbd32892ccc72a61
Thomas Metais <thomas.metais@etu.univ-nantes.fr>
2025-12-04 22:24:21 +0100

    commentaires pour aider guigui

--- repertoire.cpp
+++ repertoire.cpp
@@ -1,29 +1,32 @@
 #include "repertoire.h"
 
-elementListe* ajouter(personne &p1, elementListe* listeElement){
+elementListe *ajouter(personne &p1, elementListe *listeElement)
+{
     elementListe *teteListe = listeElement;
     elementListe *actuel = listeElement;
-    while (actuel != nullptr) {
+    while (actuel != nullptr)
+    {
         if (egalitePersonne(p1, actuel->p))
         {
             return teteListe;
         }
         // Si p1 est devant actuel->p
-        if(!comparerPersonne(p1,actuel->p)){
-            elementListe* newElement = new elementListe;
+        if (!comparerPersonne(p1, actuel->p))
+        {
+            elementListe *newElement = new elementListe;
             newElement->p = p1;
-            newElement->suivant = actuel;
-            newElement->precedent = actuel->precedent;
+            newElement->suivant = actuel;              // p1 est avant actuel
+            newElement->precedent = actuel->precedent; // p1 est après actuel->prec
             if (actuel->precedent != nullptr)
             {
-                actuel->precedent->suivant = newElement;
+                actuel->precedent->suivant = newElement; // oui
             }
-            actuel->suivant = newElement;
+            actuel->suivant = newElement; // ligne de fdp
             return teteListe;
         }
         else if (actuel->suivant == nullptr)
         {
-            elementListe* newElement = new elementListe;
+            elementListe *newElement = new elementListe;
             newElement->p = p1;
             newElement->suivant = nullptr;
             newElement->precedent = actuel;
